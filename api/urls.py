ce4a8cec5d4dff4f973bccb720264f576d7a0627
Alexis Ferrere <alexferrere@outlook.fr>
2025-03-06 12:15:21 +0100

    adding profile

--- api/urls.py
+++ api/urls.py
@@ -5,12 +5,12 @@ from .views import CardViewSet, CollectionViewSet, UserViewSet, SetViewSet, Favo
 router = DefaultRouter()
 router.register(r'cards', CardViewSet)
 router.register(r'collections', CollectionViewSet, basename='collection')
-router.register('users', UserViewSet)
-router.register('sets', SetViewSet, basename='set')
-router.register('favorites', FavoritesViewSet, basename='favorites')
+router.register(r'users', UserViewSet)
+router.register(r'sets', SetViewSet, basename='set')
+router.register(r'favorites', FavoritesViewSet, basename='favorites')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('user/profile/', UserViewSet.as_view({'get': 'profile'}), name='user-profile'),
+    path('user/profile/data/', UserViewSet.as_view({'get': 'profile_data'}), name='user-profile-data'),
 ]
-
-# urlpatterns = router.urls
